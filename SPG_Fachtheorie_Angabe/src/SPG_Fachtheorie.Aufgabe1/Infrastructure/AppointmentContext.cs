a51d95249c39c9e3204dec77c55845b6efbaef57
Daniel Wagner <WAG20331@spengergasse.at>
2025-02-21 17:14:59 +0100

    01_efcore_wiederholung

---
+++ SPG_Fachtheorie_Angabe/src/SPG_Fachtheorie.Aufgabe1/Infrastructure/AppointmentContext.cs
@@ -0,0 +1,28 @@
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using SPG_Fachtheorie.Aufgabe1.Model;
+
+namespace SPG_Fachtheorie.Aufgabe1.Infrastructure
+{
+    public class AppointmentContext : DbContext
+    {
+        // TODO: Add your DbSets here
+        public DbSet<CashDesk> CashDesks => Set<CashDesk>();
+        public DbSet<Employee> Employees => Set<Employee>();
+        public DbSet<Cashier> Cashiers => Set<Cashier>();
+        public DbSet<Manager> Managers => Set<Manager>();
+        public DbSet<Payment> Payments => Set<Payment>();
+        public DbSet<PaymentItem> PaymentItems => Set<PaymentItem>();
+
+        public AppointmentContext(DbContextOptions options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // TODO: Add your configuration here
+            modelBuilder.Entity<Employee>().HasDiscriminator(e => e.Type);
+            modelBuilder.Entity<Employee>().OwnsOne(e => e.Address);
+        }
+    }
+}
